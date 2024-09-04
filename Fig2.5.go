092f60604e356f186b6cd9a107b3aa12a7a6bceb
ABDULHAMEED IDRIS ADEDAMOLA <36794582+idris0001@users.noreply.github.com>
2024-09-04 22:40:53 +0900

    Add files via upload

---
+++ Fig2.5.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// Print the result
+	//fmt.Print("Welcome to\n" + "Go Programming\n; do enjoy the day\nfriends")
+	fmt.Print("Welcome\nto\nGo Programming Language!\nDo enjoy the day\nfriends")
+	//fmt.Printf("Go Programming! It seems it no difference between print and println here ")
+}
